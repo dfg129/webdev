abb6dcd8bb6d5c0e76e447b1980fee4ec9839bcc
dfg129 <dfg129@gmail.com>
2014-09-16 23:11:11 -0400

    Update styles

--- app_src/scripts/app.js
+++ app_src/scripts/app.js
@@ -4,7 +4,7 @@ var app = angular.module('app', ['ui.router', 'ui.grid', 'ui.grid.resizeColumns'
     $urlRouterProvider.otherwise('/home');
                                   
     var home = {
-      name: 'home',
+      name: 'Home',
       url: '/home',
       templateUrl: '/partials/home.html'
 
@@ -15,20 +15,34 @@ var app = angular.module('app', ['ui.router', 'ui.grid', 'ui.grid.resizeColumns'
           templateUrl: '/partials/preferences.html'
     },
     pods = {
-          name: 'pods',
+          name: 'Pods',
           url: '/pods',
           templateUrl: '/partials/pods.html',
           controller: 'PodsCtrl'
+   },
+   customers = {
+          name: 'Customers',
+          url: '/customers',
+          templateUrl: '/partials/customers.html',
+          controller: 'CustomersCtrl'
+   },
+   operators = {
+          name: 'Operators',
+          url: '/operators',
+          templateUrl: '/partials/operators.html',
+          controller: 'OperatorsCtrl'
    };
     $stateProvider.state(home);
     $stateProvider.state(preferences);
     $stateProvider.state(pods);
+    $stateProvider.state(customers);
+    $stateProvider.state(operators);
   })
 .controller('NavCtrl', ['$scope', function($scope) {
   $scope.navSet = {
     navMenus:[
       { navMenuItem: 'Home', subMenuItems: [{ item: 'Preferences'}] },
-      { navMenuItem: 'System', subMenuItems: [{item: 'Pods'}, {item: 'Snapshots'}, {item: 'tasks'}]}, 
+      { navMenuItem: 'System', subMenuItems: [{item: 'Pods'}, {item: 'Snapshots'}, {item: 'Tasks'}]}, 
       { navMenuItem: 'Customers' }, 
       { navMenuItem: 'Operators' },
       { navMenuItem: 'Logout'}
